d61dfcf255a41650df442bb328c6e196b06eff5f
ironLambda <rxm4946@g.rit.edu>
2017-11-28 12:26:29 -0500

    Added buffering and changed audio gen

--- PicAud.py
+++ PicAud.py
@@ -5,27 +5,29 @@ from scipy import signal
 
 # Get array of notes (C-1 to G9 Equal temperament)
 Notes = np.load("Notes.npy")
+CONLEN = np.linspace(0, .0625, int(92000*.0625), endpoint=False)
 
 
 # Called by CVAudio, takes in picture as array and uses pixel data to create waveforms
 def picToAud(array):
+    i = 0
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=92000, output=True)
+    size = len(array)
+    while size % 3 != 0:
+        array.append(np.zeros(len(array[0])))
+        size = len(array)
+    starts = [0, size//3, 2*size//3]
+    end = starts[1]
+    while i < end:
+        wave1 = genWave(array[starts[0] + i])
+        wave2 = genWave(array[starts[1] + i])
+        wave3 = genWave(array[starts[2] + i])
+        i += 1
+        sum([wave1, wave2, wave3])
+        stream.write(wave_data.astype(np.int8))
 
-    for x in array:
-        for y in x:
-            # The red value of the pixel is used to control the frequency, the green controls the length, and the blue
-            # is used to control the volume of the note
-            r = getFreq((y[2] % 64))
-            g = np.linspace(0, y[1] / 128, 92000, endpoint=False)
-            b = (y[0] / 3)
-            # Note: openCV returns pixel data in the order of blue green red
 
-            # Generate wave
-            wave_data = signal.square(2 * math.pi * r * g) * b
-
-            # Write to pyaudio stream
-            stream.write(wave_data.astype(np.int8))
 
     # Cleanup
     stream.stop_stream()
@@ -33,19 +35,18 @@ def picToAud(array):
     p.terminate()
 
 
-# Function that gets frequency value from array of frequencies, index of frequency is corresponding midi note
-def getFreq(num):
-    return Notes[num]
+def genWave(arr):
+        r = 0
+        b = 0
+        wave_data = []
+        for y in arr:
+            r = Notes[y[2] % 64]
+            b = (y[0] / 3)
 
-def noteTest(frequency):
-	l = np.linspace(0, 2, 384000, endpoint=False)
-	p = pyaudio.PyAudio()
-	stream = p.open(format=pyaudio.paInt16, channels=1, rate=192000, output=True)
-	wave_data = signal.square(2 * math.pi * frequency * l)
-	stream.write(wave_data)
+        wave_data = signal.square(2 * math.pi * r * CONLEN) * b
 
-def playNotes():
-	if __name__ == "__main__":
-		multiprocessing.Process(target = noteTest, args = [523.25113060119]).start()
-		multiprocessing.Process(target = noteTest, args = [659.25511382575]).start()
-		multiprocessing.Process(target = noteTest, args = [783.99087196355]).start()
+        return wave_data
+
+
+
+
