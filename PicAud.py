f6970dfe079839a07cc93e5cd0a40183c210b3e0
ironLambda <rxm4946@g.rit.edu>
2017-11-29 21:04:07 -0500

    Buffering rebuilt for arbitrarily large images

--- PicAud.py
+++ PicAud.py
@@ -5,30 +5,34 @@ from scipy import signal
 
 # Get array of notes (C-1 to G9 Equal temperament)
 Notes = np.load("Notes.npy")
-CONLEN = np.linspace(0, 1, int(92000*1), endpoint=False)
+CONLEN = np.linspace(0, .0625, int(92000*.0625), endpoint=False)
 
 
 # Called by CVAudio, takes in picture as array and uses pixel data to create waveforms
 def picToAud(array):
-    i = 0
+    start = 0
     p = pyaudio.PyAudio()
-    array = np.random.randint(48, 61, (6, 3, 3))
+    #array = np.random.randint(48, 61, (6, 300, 3))
+    array = np.full((6, 300), 48)
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=92000, output=True)
     size = len(array)
-    print(size)
     while size % 3 != 0:
         array = np.append(array, [np.zeros(len(array[0]))])
         size = len(array)
     starts = [0, size//3, 2*size//3]
+    data1 = array[0:starts[1]]
+    data2 = array[starts[1]:starts[2]]
+    data3 = array[starts[2]:]
     end = starts[1]
     wave_data = []
-    while i < end:
-        wave1 = genWave(array[starts[0] + i])
-        wave2 = genWave(array[starts[1] + i])
-        wave3 = genWave(array[starts[2] + i])
-        i += 1
-        wave_data = sum([wave1, wave2, wave3])
-        stream.write(wave_data.astype(np.int8))
+    for i in range(len(data1)):
+        while start < len(data1[0]):
+            wave1 = genWave(data1[i][start:start+32])
+            wave2 = genWave(data2[i][start:start+32])
+            wave3 = genWave(data3[i][start:start+32])
+            start += 32
+            wave_data = sum([wave1, wave2, wave3])
+            stream.write(wave_data.astype(np.int8))
 
 
 
@@ -41,13 +45,15 @@ def picToAud(array):
 def genWave(arr):
         r = 0
         b = 0
+        i = 0
         wave_data = np.array([])
         for y in arr:
-            r = Notes[y[2] % 64]
-            b = (y[0] / 3)
+            print(i)
+            r = Notes[y % 64]
+            b = 60
             note = signal.square(2 * math.pi * r * CONLEN) * b
             wave_data = np.append(wave_data, [note], axis=None)
-
+            i+=1
         return wave_data
 
 
