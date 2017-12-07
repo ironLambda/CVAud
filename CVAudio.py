bbf1984992f10147963adb4a6af67521e99839fa
Ryan Marlin <rxm4946@g.rit.edu>
2017-12-07 12:10:43 -0500

    Final for project files
    We finished the project

--- CVAudio.py
+++ CVAudio.py
@@ -3,7 +3,13 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import PicAud as p
+import pyaudio
+from threading import Thread
+from queue import Queue 
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -15,14 +21,28 @@ ap.add_argument("-i", "--image", help="Path to an image file")
 args = vars(ap.parse_args())
 
 # Define the Color range we are tracking
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenLower = (12, 83, 0)
+greenUpper = (42, 255, 255)
 
 # Set up variables
 pts = deque(maxlen=args["buffer"])
 counter = 0
-(dX, dY) = (0, 0)
-direction = ""
+pyaud = pyaudio.PyAudio()
+stream = pyaud.open(format=pyaudio.paInt16, channels=1, rate=4000, output=True)
+waves = Queue()
+
+
+def playthread(waves):
+    
+    for wave in iter(waves.get, None):
+        
+        print(wave)
+        stream.write(wave.astype(np.int8))
+        
+    
+        
+thread = Thread( target=playthread, args=(waves,))
+thread.start()
 
 # run image program if flag used
 if args.get("image", False):
@@ -31,19 +51,21 @@ if args.get("image", False):
 
 # If video supplied, use video, otherwise use webcam
 if not args.get("video", False):
-    camera = cv2.VideoCapture(0)
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 60
+    raw = PiRGBArray(camera, size=(640, 480))
+    time.sleep(0.1)
 else:
     camera = cv2.VideoCapture(args["video"])
 
-
-while 1:
-    # Get first frame
-    grab, frame = camera.read()
-    if args.get("video") and not grab:
-        break
+n = 0
+v = 0
+    
+for rawim in camera.capture_continuous(raw, format="bgr", use_video_port = True):
 
     # make frame smaller, in the right direction, and in the right color space
-    frame = imutils.resize(frame, width=800)
+    frame = rawim.array
     cv2.flip(frame, 1, frame)
     blur = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -56,6 +78,7 @@ while 1:
     # select the green object
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
+    
 
     # Draw circle around object
     if len(cnts) > 0:
@@ -64,8 +87,8 @@ while 1:
         M = cv2.moments(c)
 
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-        print(center)
+        
+        n, v = center
 
         if radius > 10:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
@@ -73,39 +96,17 @@ while 1:
             pts.appendleft(center)
 
     # figure out movement direction
-    for i in np.arange(1,len(pts)):
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        if counter >= 10 and i == 1 and pts[-10] is not None:
-            dX = pts[-10][0] - pts[i][0]
-            dY = pts[-10][1] - pts[i][1]
-            dirX, dirY = ("", "")
-
-            if np.abs(dX) > 20:
-                dirX = "West" if np.sign(dX) == 1 else "East"
-            if np.abs(dY) > 20:
-                dirY = "North" if np.sign(dY) == 1 else "South"
-            if dirX != "" and dirY != "":
-                direction = "{}-{}".format(dirY, dirX)
-            else:
-                direction = dirX if dirX != "" else dirY
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-    # Write text on the screen
-    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                0.65, (0, 0, 255), 3)
-    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                0.35, (0, 0, 255), 1)
-
-    # Show frame
-    cv2.imshow("Frame", frame)
+    wave = p.genWaveRealtime([v, n])
+    stream.write(wave.astype(np.int8))
+    
+    cv2.imshow("Frame",frame)
     key = cv2.waitKey(1) & 0xFF
     counter += 1
 
     if key == ord("q"):
         break
+    raw.truncate(0)
+    
 
 
 # Cleanup
--- PicAud.py
+++ PicAud.py
@@ -2,60 +2,73 @@ import numpy as np
 import pyaudio
 import math
 from scipy import signal
-import matplotlib.pyplot as plt
+from threading import Thread
+import time
 
 # Get array of notes (C-1 to G9 Equal temperament)
 Notes = np.load("Notes.npy")
-CONLEN = np.linspace(0, 1, 4000, endpoint=False)
-
+CONLEN = np.linspace(0, .3, 28800, endpoint=False)
+CONLEN2 = np.linspace(0, .02, 1920, endpoint=False)
+waves = []
 
 # Called by CVAudio, takes in picture as array and uses pixel data to create waveforms
 def picToAud(array):
-    start = 0
+    global waves
     p = pyaudio.PyAudio()
-    #array = np.random.randint(48, 61, (6, 300, 3))
-    array = np.full((6, 300, 3), 60)
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=4000, output=True)
+    
     size = len(array)
     while size % 3 != 0:
         array = np.append(array, [np.zeros(len(array[0]))])
         size = len(array)
+    
     starts = [0, size//3, 2*size//3]
-    data1 = array[0:starts[1]]
-    data2 = array[starts[1]:starts[2]]
-    data3 = array[starts[2]:]
-    end = starts[1]
-    wave_data = []
-    for i in range(len(data1)):
-        while start < len(data1[0]):
-            wave1 = genWave(data1[i][start:start+2])
-            wave2 = genWave(data2[i][start:start+2])
-            wave3 = genWave(data3[i][start:start+2])
-            start += 2
-            wave_data = sum([wave1, wave2, wave3])
-            stream.write(wave_data.astype(np.int8))
-
-
-
-    # Cleanup
+    proc = Thread(target = genWaveProc, args=(array[0:starts[1]],array[starts[1]:starts[2]],array[starts[2]:],))
+    proc.start()
+    time.sleep(2)
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=96000, output=True)
+    for wave in waves:
+        stream.write(wave.astype(np.int8))
     stream.stop_stream()
     stream.close()
     p.terminate()
 
+    
+
+
+def genWaveRealtime(arr):
+    v = 12
+    n = Notes[int((arr[1]/640) * 128)]
+    print(n)
+    note = signal.square(2 * math.pi * n * CONLEN2)*v
+    return note.astype(np.int16)
 
 def genWave(arr):
         r = 0
         b = 0
         wave_data = np.array([])
         for y in arr:
-            r = Notes[y[2] % 64]
-            b = y[0] / 3
+            r = Notes[y[2] % 128]
+            b = y[0] 
             note = signal.square(2 * math.pi * r * CONLEN) * b
             wave_data = np.append(wave_data, [note], axis=None)
 
-        plt.plot(wave_data)
-        plt.show()
+        
         return wave_data.astype(np.int16)
+    
+def genWaveProc(ar1, ar2, ar3):
+    global waves
+    start = 0
+    for i in range(len(ar1)):
+        while start < len(ar1[0]):
+            wave1 = genWave(ar1[i][start:start+2])
+            wave2 = genWave(ar2[i][start:start+2])
+            wave3 = genWave(ar3[i][start:start+2])
+            start += 2
+            wave_data = sum([wave1, wave2, wave3])
+            print(wave_data)
+            waves.append(wave_data)
+            
+        
+    
 
 
-picToAud([])
